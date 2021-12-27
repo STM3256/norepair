3ffad67a36ad68ed484bac2c84986e89838da7f6
Stephen Miller <stephen.miller7@gmail.com>
2021-12-26 22:29:12 -0500

    added some roleplay meme and logo

--- main.lua
+++ main.lua
@@ -1,24 +1,18 @@
 -- https://wowpedia.fandom.com/wiki/World_of_Warcraft_API
 -- https://developer.school/developing-world-of-warcraft-addons-hello-world-part-one
 -- https://wowwiki-archive.fandom.com/wiki/Events_A-Z_(full_list)
---message?
-print('I can haxxor code3!')
 
 local function DisableRepair(self, event)
     --check if vendor is a repairer
     if(CanMerchantRepair()) then
         local repairAllCost, canRepair = GetRepairAllCost();
-        DEFAULT_CHAT_FRAME:AddMessage('this vendor can repair')
-        DEFAULT_CHAT_FRAME:AddMessage('repair cost='..GetCoinTextureString(repairAllCost)..',  canRepair='..tostring(canRepair))
-        DEFAULT_CHAT_FRAME:AddMessage(canRepair)
+        DEFAULT_CHAT_FRAME:AddMessage('Mom: It costs HOW MUCH to repair your gear? '..GetCoinTextureString(repairAllCost))
+        DEFAULT_CHAT_FRAME:AddMessage('Mom: We can repair at home. Lets go.')
         CloseMerchant()
     end
 end
 
--- create frame
 local event = CreateFrame("Frame")
-
---register the event
 event:SetScript("OnEvent", DisableRepair);
 event:RegisterEvent("MERCHANT_SHOW")
 
